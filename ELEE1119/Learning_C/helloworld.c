98dc7f5e552323c8faec5e0fab47c298d7ff239d
sameyr <ss1366t@gre.ac.uk>
2023-11-26 20:50:31 +0000

    beginning of C

---
+++ ELEE1119/Learning_C/helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //standard input output library
+
+int main()
+{
+	printf("Hello World\n"); //printf prints the line
+	printf("GoodBye World\n"); 
+	return 0; // terminates the program by returnning 0 to int main
+}
+
