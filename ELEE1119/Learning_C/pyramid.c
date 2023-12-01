547a506ad1f684a9cff81f9d7b2b61628452e176
sameyr <ss1366t@gre.ac.uk>
2023-12-01 15:58:45 +0000

    code added

--- ELEE1119/Learning_C/pyramid.c
+++ ELEE1119/Learning_C/pyramid.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
 int main(){
-	for(int i =1 ; i<10; i++){
+	//pyramid
+	/*for(int i =1 ; i<10; i++){
 		for(int j=1 ; j<i;j++){
-		printf("* ");	
+		printf("*");	
+		}
+		printf("\n");
+	}*/
+
+	// reverse pyramid
+	for(int i=8;i>0;i--){
+		for(int j =i;j>0;j--){
+			printf("*");	
 		}
 		printf("\n");
 	}
+	
 }
